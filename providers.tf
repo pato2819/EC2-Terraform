579cd755e22eb3d537a00f217d64b2bd4bd170d6
pato2819 <patycb02@gmail.com>
2023-04-09 13:58:44 -0600

    edit variables in providers file

--- providers.tf
+++ providers.tf
@@ -15,7 +15,7 @@ terraform {
 provider "aws" {
   
   region  = var.aws_region
-  access_key = "AWS_ACCESS_KEY_ID"
-  secret_key = "AWS_SECRET_ACCESS_KEY"
+  access_key = "${env.AWS_ACCESS_KEY_ID}"
+  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
   profile = "default"
 }
